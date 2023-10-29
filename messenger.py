a9dbf20caf4568f92cce9215d14db28db72ccf83
Adin Zimmerman <adinz@email.unc.edu>
2023-10-29 14:07:14 -0400

    changes

--- messenger.py
+++ messenger.py
@@ -1,6 +1,9 @@
 import os
 import pickle
 import string
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
+
 
 class MessengerServer:
     def __init__(self, server_signing_key, server_decryption_key):
@@ -12,8 +15,9 @@ class MessengerServer:
         return
 
     def signCert(self, cert):
-        raise Exception("not implemented!")
-        return
+        ecdsa = ec.ECDSA(hashes.SHA256())
+        sig = self.server_signing_key.sign(cert, ecdsa)
+        return sig
 
 class MessengerClient:
 
@@ -23,19 +27,23 @@ class MessengerClient:
         self.server_encryption_pk = server_encryption_pk
         self.conns = {}
         self.certs = {}
-        self.secret_key = None
+        self.private_key = None
         
 
     def generateCertificate(self):
+        self.private_key = ec.generate_private_key(ec.SECP256K1())
+        pk = self.private_key.public_key()
+        return Certificate(pk, self.name)
 
-        raise Exception("not implemented!")
-        return
-
-    def receiveCertificate(self, certificate, signature):
-        raise Exception("not implemented!")
+    def receiveCertificate(self, certificate, foreign_signature):
+        ecdsa = ec.ECDSA(hashes.SHA256())
+        self.server_signing_pk.verify(foreign_signature,certificate, ecdsa)
+        self.certs[certificate.name] = [self.private_key.exchange(ec.ECDH, certificate.publickey), certificate.publickey]
         return
 
     def sendMessage(self, name, message):
+        if name not in self.conns:
+            return
         raise Exception("not implemented!")
         return
 
